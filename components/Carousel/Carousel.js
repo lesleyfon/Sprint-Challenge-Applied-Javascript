d7b66f59f383bfd36638a29845247dc50ecdce6f
lesleyfon <lesleybanadzem2017@gmail.com>
2019-08-17 14:48:44 -0700

    completed stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,57 @@
     <div class="right-button"> > </div>
   </div>
 */
+let imgArr = [
+  './assets/carousel/mountains.jpeg',
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg"
+]
+let index = 0;
+// funtion that return image array with an index
+function nextImage(){
+  return imgArr[index]
+}
+
+function carouselComponent(){
+  const carousel = document.createElement('div');
+  carousel.classList.add('carousel');
+    //left Button
+  const leftCarouselButton = document.createElement('div');
+  leftCarouselButton.classList.add('left-button');
+  carousel.appendChild(leftCarouselButton);
+  const img =  document.createElement('img'); 
+  img.src = nextImage()
+  carousel.appendChild(img);
+  // click event for the left button
+  leftCarouselButton.addEventListener('click', e=>{
+    e.preventDefault();
+    // track if index is less equal to 0;
+    if(index === 0){
+      // if it is then we want to return
+      return;
+    } else{
+      // else we want to reduce the index ny one 
+      index--;
+    } 
+    //we just run the next image function which returns index array of a particular index
+    img.src = nextImage();
+  }) 
+ 
+  //right button
+  const rightCarouselButton = document.createElement('div');
+  rightCarouselButton.classList.add('right-button');
+  carousel.appendChild(rightCarouselButton);
+  rightCarouselButton.addEventListener('click', e=>{
+    e.preventDefault();
+    if(index === 3){
+      return 
+    } else{
+      index++
+    } 
+    img.src = nextImage()
+    console.log(index)
+  }) 
+  return carousel
+}
+const carouselContainer = document.querySelector(".carousel-container");carouselContainer.appendChild(carouselComponent())
